b39e0b2601e1f8fb0bcf86a62455fb063e99e625
Artem Musalitin <artem@musalitin.me>
2025-04-26 00:14:53 +0200

    Add config file and customization options

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"log"
 	"net"
 	"os"
-	"time"
+	"path/filepath"
+	"strings"
+	"sync"
 
 	"github.com/getlantern/systray"
+	"gopkg.in/yaml.v3"
 )
 
 type LayerChangeMsg struct {
@@ -19,26 +22,135 @@ type LayerChangeMsg struct {
 	} `json:"LayerChange"`
 }
 
+type LabelConfig struct {
+	Text   *string `yaml:"text,omitempty"`
+	Hidden bool    `yaml:"hidden"`
+}
+
+type IconConfig struct {
+	Path *string `yaml:"path,omitempty"`
+}
+
+type LayerConfig struct {
+	Label LabelConfig `yaml:"label"`
+	Icon  IconConfig  `yaml:"icon"`
+}
+
+type Config struct {
+	Host   string                 `yaml:"host"`
+	Port   int                    `yaml:"port"`
+	Layers map[string]LayerConfig `yaml:"layers"`
+}
+
+var (
+	configPath   string
+	config       Config
+	configLock   sync.RWMutex
+	currentLayer string
+	iconsByLayer map[string][]byte
+)
+
 //go:embed icon.png
-var iconData []byte
+var defaultIcon []byte
 
-var currentLayer string
+func loadInitialConfig() {
+	candidates := []string{
+		filepath.Join(os.Getenv("HOME"), ".config", "kanata-layer-monitor", "config.yaml"),
+		filepath.Join(os.Getenv("HOME"), ".config", "kanata-layer-monitor", "config.yml"),
+	}
 
-func main() {
-	systray.Run(onReady, onExit)
+	for _, path := range candidates {
+		if _, err := os.Stat(path); err == nil {
+			configPath = path
+			break
+		}
+	}
+
+	if configPath != "" {
+		if err := loadConfig(configPath); err != nil {
+			log.Printf("Failed to load config: %v", err)
+		}
+	} else {
+		log.Println("No config found, using defaults")
+	}
+}
+
+func loadIcon(path string) (icon []byte) {
+	iconPath := filepath.Join(filepath.Dir(configPath), path)
+	icon, err := os.ReadFile(iconPath)
+
+	if err != nil {
+		log.Printf("Failed to load icon '%s': %v", iconPath, err)
+	}
+
+	return icon
+}
+
+func loadConfig(path string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	ext := strings.ToLower(filepath.Ext(path))
+
+	if ext != ".yaml" && ext != ".yml" {
+		log.Println("Config format not supported. Use yaml or yml. Using default configuration")
+	}
+
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return err
+	}
+
+	if config.Host == "" {
+		config.Host = "127.0.0.1"
+	}
+
+	if config.Port == 0 {
+		config.Port = 4444
+	}
+
+	log.Printf("Config loaded from %s", path)
+
+	return nil
+}
+
+func loadIcons() {
+	iconCache := make(map[string][]byte)
+
+	for layer, layerConfig := range config.Layers {
+		if layerConfig.Icon.Path != nil {
+			if icon := loadIcon(*layerConfig.Icon.Path); icon != nil {
+				iconCache[layer] = icon
+				log.Printf("Icon for layer %s loaded", layer)
+			}
+		}
+	}
+
+	iconsByLayer = iconCache
 }
 
 func onReady() {
-	systray.SetTemplateIcon(iconData, iconData)
-	systray.SetTitle("...")
+	systray.SetIcon(defaultIcon)
+	changeShowedLayer("...")
+
 	systray.SetTooltip("Kanata Layer Monitor")
+
+	connectedInfo := fmt.Sprintf("Listening %s:%d", config.Host, config.Port)
+	info := systray.AddMenuItem(connectedInfo, connectedInfo)
+	info.Disable()
+
+	systray.AddSeparator()
 	quitItem := systray.AddMenuItem("Quit", "Quit the application")
 
 	go quitChecker(*quitItem)
 	go monitorLayer()
 }
 
-// Listen for menu item click and exit when Quit is clicked
+func onExit() {
+	// Cleanup if needed
+}
+
 func quitChecker(quitItem systray.MenuItem) {
 	<-quitItem.ClickedCh
 	log.Printf("Shutting down")
@@ -47,42 +159,93 @@ func quitChecker(quitItem systray.MenuItem) {
 	os.Exit(0)
 }
 
-func onExit() {
-	// Cleanup if needed
+func monitorLayer() {
+	for {
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", config.Host, config.Port))
+		if err != nil {
+			log.Printf("Connection failed: %v", err)
+			changeShowedLayer("Error")
+			return
+		}
+
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			line := scanner.Bytes()
+
+			var msg LayerChangeMsg
+			if err := json.Unmarshal(line, &msg); err != nil {
+				log.Printf("Invalid JSON: %s", line)
+				changeShowedLayer("N/A")
+				continue
+			}
+
+			layer := msg.LayerChange.New
+			if layer != currentLayer {
+				currentLayer = layer
+				changeShowedLayer(layer)
+			}
+		}
+		conn.Close()
+	}
 }
 
-func monitorLayer() {
-	conn, err := net.Dial("tcp", "127.0.0.1:4444")
-	if err != nil {
-		log.Printf("Failed to connect: %v", err)
-		systray.SetTitle("Error")
+func changeShowedLayer(layerName string) {
+	configLock.RLock()
+	defer configLock.RUnlock()
+
+	layerConfig, ok := config.Layers[layerName]
+	text := layerName
+	icon := defaultIcon
+
+	if ok {
+		if layerConfig.Label.Text != nil {
+			text = *layerConfig.Label.Text
+		}
+		if iconsByLayer[layerName] != nil {
+			icon = iconsByLayer[layerName]
+		}
+	}
+
+	if layerName != "N/A" && layerName != "Error" && layerConfig.Label.Hidden == true {
+		text = ""
+	}
+
+	systray.SetTitle(fmt.Sprintf(" %s", text))
+	systray.SetIcon(icon)
+
+	writeToFile(text)
+
+	log.Printf("Layer changed to \"%s\" (%s)", text, layerName)
+}
+
+func writeToFile(layer string) {
+	dirPath := filepath.Join(os.Getenv("HOME"), ".cache", "kanata-layer-monitor")
+	filePath := filepath.Join(dirPath, "current-layer")
+
+	// Create the directories (if they don't exist)
+	if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
+		fmt.Println("Error creating directories:", err)
 		return
 	}
-	defer conn.Close()
 
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		line := scanner.Bytes()
+	// Open the file with O_RDWR|O_CREATE|O_TRUNC flags (rewrite)
+	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0640)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
 
-		var msg LayerChangeMsg
-		if err := json.Unmarshal(line, &msg); err != nil {
-			log.Printf("Invalid JSON: %s\nError: %v\n", line, err)
-			continue
-		}
-
-		layer := msg.LayerChange.New
-		if layer != currentLayer {
-			currentLayer = layer
-			title := fmt.Sprintf("%s", currentLayer)
-			systray.SetTitle(title)
-			log.Printf("Kanata layer changed to %s", title)
-		}
+	_, err = file.WriteString(layer)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Printf("Scanner error: %v", err)
-		systray.SetTitle("N/A")
-		time.Sleep(5 * time.Second)
-		os.Exit(1)
-	}
+}
+
+func main() {
+	loadInitialConfig()
+	loadIcons()
+	systray.Run(onReady, onExit)
 }
